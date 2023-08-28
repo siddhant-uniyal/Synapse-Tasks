9391dc41dc07fbe6fd5486e402fed8ee645b1199
Siddhant Uniyal <110982730+siddhant-uniyal@users.noreply.github.com>
2023-08-28 13:05:53 +0530

    Removed a for loop

--- ml_task_1.2.py
+++ ml_task_1.2.py
@@ -5,32 +5,26 @@ given_list = ['0001','0011','0101','1011','1101','1111']
 
 convert = lambda x : int(x,2)
 
-converted_list =  list(map(convert,given_list))
+converted_list = list(map(convert,given_list))
 
 total = sum(converted_list)
-half_sum = int(total/2)
 
-def subset_sums(nums):
-    result = []
-    for r in range(len(nums) + 1):
-        for subset in combinations(nums, r):
-            result.append(sum(subset))
-    return result
+half_sum = int(total/2) #ideal solution for a list of sum n , is [n/2,n/2] OR [n+1/2,n-1/2]. We use half_sum as a filter for the best solution
+                       
+def best_sum(nums):
+    check = sys.maxsize
+    for r in range(1 , len(nums) + 1): #chooses length of subset , starting from 1 till length of list
+        for subset in combinations(nums, r): #creates the subset
+            result = sum(subset)
+            difference = abs(result-half_sum) #checking how far off the sum of the current subset is from (total of list)/2
+            if(difference<check): #true for first step
+                check = difference  #every iteration starting from the second will be checked against the previous one
+                best = result # the best sum is the one closest to total/2
+    return best 
 
-check = sys.maxsize
+last_elem = best_sum(converted_list)
 
-last_elem = 0
+req_list = [last_elem , total - last_elem]
 
-sums = subset_sums(converted_list)
-
-for count,value in enumerate(sums):
-    if(abs(value-half_sum)<check):
-        check = abs(value-half_sum)
-        last_elem = value
-
-req_list = [last_elem,total-last_elem]
-
-print(req_list) 
-
-
+print(req_list)
 
